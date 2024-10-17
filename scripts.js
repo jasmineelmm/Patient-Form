fe149a82e5d1b514d78dc49d297f3bc2d5011b43
jasmineelmm <128508173+jasmineelmm@users.noreply.github.com>
2024-10-17 18:12:01 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
-// Set the current date in the header
+/* 
+ Program:   Simple Sample Registration Form Validation 
+ Author:    Jasmine Elmiloudi
+ Date Modified: 2024-10-17
+ Description: This JavaScript file contains functions for 
+              validating form inputs, handling events, 
+              and displaying user feedback based on 
+              their input. 
+*/
+
+ // Set the current date in the header
 document.addEventListener("DOMContentLoaded", function () {
     const currentDate = new Date().toLocaleDateString();
     document.getElementById("currentDate").textContent = `Today is: ${currentDate}`;
